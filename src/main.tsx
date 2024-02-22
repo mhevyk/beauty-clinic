fdb2516c210facbc7903191ee73939bf348e9868
Vasya <feniak2003@gmail.com>
2024-02-22 20:39:22 +0200

    add theme folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import router from "./router.tsx";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import Theme from "./theme/Theme.tsx";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <RouterProvider router={createBrowserRouter(router)} />
+      <ThemeProvider theme={Theme}>
+          <CssBaseline/>
+          <RouterProvider router={createBrowserRouter(router)} />
+      </ThemeProvider>
   </StrictMode>,
 );

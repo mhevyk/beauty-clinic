db5a15623cec36cf3e754ad3339b4252352e70e7
Vasya <feniak2003@gmail.com>
2024-02-23 11:31:53 +0200

    add color theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,5 +1,18 @@
+import {createTheme} from "@mui/material";
 
-const theme = {
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#000000',
+        },
+        secondary: {
+            main: '#f8ebe1',
+        },
+        background: {
+            paper: '#ffffff',
+        },
+    },
+});
 
-}
 export default theme;

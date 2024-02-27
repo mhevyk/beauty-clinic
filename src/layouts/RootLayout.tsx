d360bbed00b9e231b80251872358e3bbb4638b17
mhevyk <maxxevyk7@gmail.com>
2024-02-27 20:12:37 +0200

    Added scroll restoration

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "./Navbar";
 
 export default function RootLayout() {
   return (
     <>
+      <ScrollRestoration />
       <Navbar />
       <Outlet />
     </>

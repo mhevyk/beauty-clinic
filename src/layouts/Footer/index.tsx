02f5d0f34dff7d90f3cf2c24332dbd926dd8ce69
mhevyk <maxxevyk7@gmail.com>
2024-03-14 22:20:42 +0200

    Added copyright text

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -16,6 +16,14 @@ const ContactDetailsTitle = styled("h3")(({ theme }) => ({
   },
 }));
 
+// TODO: fix font
+const CopyrightText = styled("p")(({ theme }) => ({
+  ...theme.typography.FontAvenirLight2,
+  fontSize: "14px",
+  textAlign: "center",
+  textTransform: "uppercase",
+}));
+
 // TODO: Add scroll margin top when testimonials is completed
 export default function Footer() {
   return (
@@ -23,6 +31,7 @@ export default function Footer() {
       <ContactDetailsTitle>Let's get in touch</ContactDetailsTitle>
       <ContactDetails />
       <GoogleMap />
+      <CopyrightText>© 2023 by Maksym Hevyk and Vasyl Feniak</CopyrightText>
     </Container>
   );
 }

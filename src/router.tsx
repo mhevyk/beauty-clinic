170f94a86bcda2259218da5d2e846bd4800ca60c
mhevyk <maxxevyk7@gmail.com>
2024-03-21 14:40:38 +0200

    Added Calendar on new order page

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import RootLayout from "@layouts/RootLayout";
 import ErrorPage from "@pages/ErrorPage";
 import HomePage from "@pages/HomePage";
 import NotFoundPage from "@pages/NotFoundPage";
+import OrderPage from "@pages/OrderPage";
 
 const router: RouteObject[] = [
   {
@@ -11,6 +12,7 @@ const router: RouteObject[] = [
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: "order", element: <OrderPage /> },
       /* Add new routes here
          Example: { path: "/test", element: <MyComponent /> }
       */

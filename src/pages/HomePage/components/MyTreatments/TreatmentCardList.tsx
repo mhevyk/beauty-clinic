4a27a4b7eb15214f94e327f5617b017bd3df4ef2
Vasya <feniak2003@gmail.com>
2024-03-28 19:42:51 +0200

    add treatmentItems.ts where be style svg image for Treatment

--- src/pages/HomePage/components/MyTreatments/TreatmentCardList.tsx
+++ src/pages/HomePage/components/MyTreatments/TreatmentCardList.tsx
@@ -1,82 +1,13 @@
-import { Treatment } from "@api/hooks";
 import TreatmentCard from "@pages/HomePage/components/MyTreatments/TreatmentCard.tsx";
-import homeSignatureFacial from "@images/homeSignatureFacial.svg";
-import homeOxygenFacial from "@images/homeOxygenFacial.svg";
-import homePurifyingHerbalFacial from "@images/homePurifyingHerbalFacial.svg";
-
-// import { useGetLimitedTreatmentsSuspenseQuery } from "@api/hooks";
-
-const data: { treatments: Treatment[] } = {
-  treatments: [
-    {
-      __typename: "Treatment",
-      id: "1",
-      name: "Signature Facial",
-      duration: 90,
-      pricePerUnit: 200,
-      imageUrl:
-        "/2feeec_966aedab4a3f491f801a3ddc40ef56ca~mv2-lvQkZJo5qNd6xX7RIiSMshBS5l7XUF.webp",
-    },
-    {
-      __typename: "Treatment",
-      id: "2",
-      name: "Oxygen Facial",
-      duration: 60,
-      pricePerUnit: 160,
-      imageUrl:
-        "/2feeec_91ff7bb6e66844629569bb641280c5a9~mv2-GVD1fGVEnm9NVERaeABTTgm9ScD4mw.webp",
-    },
-    {
-      __typename: "Treatment",
-      id: "3",
-      name: "Purifying Herbal Facial",
-      duration: 60,
-      pricePerUnit: 160,
-      imageUrl:
-        "/2feeec_5b22635c22154cb59ab4603d6247654f~mv2_d_1306_1308_s_2-akOASgUHyy8nWiu3Uaheoh8YhLUo1e.webp",
-    },
-  ],
-};
-
-const svgImages = [
-  {
-    id: "1",
-    svgImage: homeSignatureFacial,
-    styled: {
-      top: "35%",
-      right: "26%",
-      height: "177px",
-      transform: "translate(-50%, -50%) rotate(349deg)",
-    },
-  },
-  {
-    id: "2",
-    svgImage: homeOxygenFacial,
-    styled: {
-      top: "28%",
-      left: "50%",
-      height: "234px",
-      transform: "translate(-50%, -50%)",
-    },
-  },
-  {
-    id: "3",
-    svgImage: homePurifyingHerbalFacial,
-    styled: {
-      top: "25%",
-      left: "35%",
-      height: "253px",
-      transform: "translate(-50%, -50%) rotate(18deg)",
-    },
-  },
-];
+import treatmentSvgImage from "@pages/HomePage/data/treatmentItems.ts";
+import { useGetLimitedTreatmentsSuspenseQuery } from "@api/hooks";
 
 export default function TreatmentCardList() {
-  // const { data } = useGetLimitedTreatmentsSuspenseQuery({
-  //   variables: { limit: 3 },
-  // });
+  const { data } = useGetLimitedTreatmentsSuspenseQuery({
+    variables: { limit: 3 },
+  });
   return data.treatments.map((treatment, index) => {
-    const svgImage = svgImages[index];
+    const svgImage = treatmentSvgImage[index];
     if (!svgImage) {
       return null;
     }
@@ -86,7 +17,7 @@ export default function TreatmentCardList() {
         key={treatment.id}
         treatment={treatment}
         svgImage={svgImage.svgImage}
-        styled={svgImage.styled}
+        style={svgImage.styled}
       />
     );
   });

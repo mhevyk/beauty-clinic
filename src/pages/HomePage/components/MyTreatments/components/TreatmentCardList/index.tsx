e482134c6ee440299dbcd803814ce912d660a536
Vasya <feniak2003@gmail.com>
2024-03-29 15:21:09 +0200

    fix import

--- src/pages/HomePage/components/MyTreatments/components/TreatmentCardList/index.tsx
+++ src/pages/HomePage/components/MyTreatments/components/TreatmentCardList/index.tsx
@@ -1,5 +1,5 @@
 import TreatmentCard from "../TreatmentCard.tsx";
-import myTreatmentDecorationImageData from "@pages/HomePage/components/MyTreatments/components/TreatmentCardList/data/treatmentItems.ts";
+import myTreatmentDecorationImageData from "./data/treatmentItems.ts";
 import { useGetLimitedTreatmentsSuspenseQuery } from "@api/hooks";
 
 export default function TreatmentCardList() {
@@ -10,12 +10,12 @@ export default function TreatmentCardList() {
   return data.treatments.map((treatment, index) => {
     const svgImage = myTreatmentDecorationImageData[index];
 
+    if (!svgImage) return null;
+
     const treatmentImageSrc = new URL(
       treatment.imageUrl,
       import.meta.env.VITE_API_BASE_IMAGE_URL,
     ).href;
-
-    if (!svgImage) return null;
 
     return (
       <TreatmentCard

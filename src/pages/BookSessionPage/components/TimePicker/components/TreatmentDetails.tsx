69560fea4a45467ad0834ff5ccdb48e626b33ba5
mhevyk <maxxevyk7@gmail.com>
2024-05-05 20:20:47 +0300

    Connected timepicker to backend

---
+++ src/pages/BookSessionPage/components/TimePicker/components/TreatmentDetails.tsx
@@ -0,0 +1,43 @@
+import { useGetTreatmentByIdSuspenseQuery } from "@api/hooks";
+import { Box, Typography } from "@mui/material";
+import minutesToHourAndMinutes from "@utils/minutesToHourAndMinutes";
+import { format } from "date-fns";
+import EmployeeSelect from "./EmployeeSelect";
+import { useDatetimePickerContext } from "@pages/BookSessionPage/context/DatetimePickerProvider";
+
+type TreatmentDetailsProps = {
+  hasAvailableSession: boolean;
+};
+
+export default function TreatmentDetails({
+  hasAvailableSession,
+}: TreatmentDetailsProps) {
+  const { selectedDate, treatmentId } = useDatetimePickerContext();
+
+  const { data } = useGetTreatmentByIdSuspenseQuery({
+    variables: { treatmentId: treatmentId },
+  });
+
+  const treatment = data.treatment;
+
+  return (
+    <Box paddingBottom="12px">
+      <Typography fontSize="16px">{treatment.name}</Typography>
+      <EmployeeSelect />
+      {hasAvailableSession && (
+        <>
+          <Typography fontSize="16px">
+            {format(selectedDate, `MMMM d, yyyy h:mm aaa `)}
+          </Typography>
+
+          <Typography color="#605f5d" fontSize="14px">
+            {minutesToHourAndMinutes(treatment.duration)}
+          </Typography>
+        </>
+      )}
+      <Typography color="#605f5d" fontSize="14px">
+        $ {treatment.pricePerUnit}
+      </Typography>
+    </Box>
+  );
+}

5956cd669dad000ff870ef04971cc02763131a6b
Vasya <feniak2003@gmail.com>
2024-05-09 21:48:55 +0300

    separate validation schemas

--- src/validation/common.ts
+++ src/validation/common.ts
@@ -14,3 +14,12 @@ export const passwordFieldValidation = Yup.string()
   .required("Password is required")
   .min(5, "Password must be at least 5 characters")
   .max(255, "Password must be at most 255 characters");
+
+export const phoneNumberFieldValidation = Yup.string().matches(
+  /^\d{10}$/,
+  "Phone number must be 10 digits",
+);
+
+export const messageFieldValidation = Yup.string()
+  .min(10, "Message must be at least 10 characters")
+  .max(500, "Message must be at most 500 characters");

41845c2ad56ab67ed51536b0b2103f61b1962a97
mhevyk <maxxevyk7@gmail.com>
2024-05-09 08:58:57 +0300

    Fixed bug when first available hour was not selected when employeeId changed

--- src/pages/BookSessionPage/components/TimePicker/index.tsx
+++ src/pages/BookSessionPage/components/TimePicker/index.tsx
@@ -1,11 +1,11 @@
 import { Box, Button, styled } from "@mui/material";
-import { format, subMinutes } from "date-fns";
+import { subMinutes } from "date-fns";
 import theme from "@theme/theme.ts";
-import { useEffect } from "react";
 import useLimitedSessionHours from "./hooks/useLimitedSessionHours.ts";
 import { useGetAvailableTreatmentSessionHoursSuspenseQuery } from "@api/hooks";
 import { useDatetimePickerContext } from "@pages/BookSessionPage/context/DatetimePickerProvider.tsx";
 import NoAvailability from "./components/NoAvailability.tsx";
+import TimeButton from "./components/TimeButton.tsx";
 
 const BoxGridStyled = styled(Box)({
   maxWidth: "375px",
@@ -29,31 +29,8 @@ const ButtonShowSessionStyled = styled(ButtonStyled)({
   },
 });
 
-type ButtonStyledPickerProps = {
-  isSelected: boolean;
-};
-
-const ButtonStyledPicker = styled(Button, {
-  shouldForwardProp: (prop) => prop !== "isSelected",
-})<ButtonStyledPickerProps>(({ isSelected }) => ({
-  backgroundColor: isSelected ? "#e0d9ce" : undefined,
-  color: isSelected ? "black" : undefined,
-  borderColor: isSelected ? theme.palette.secondary.main : "#66635e",
-
-  py: "8px",
-  width: "117.659px",
-  padding: "8px",
-  textAlign: "center",
-  "&:hover, &:focus": {
-    borderColor: theme.palette.secondary.main,
-    color: "black",
-    backgroundColor: "#e0d9ce",
-  },
-}));
-
 const TimePicker = () => {
-  const { selectedDate, selectedTime, setSelectedTime, selectedEmployeeId } =
-    useDatetimePickerContext();
+  const { selectedDate, selectedEmployeeId } = useDatetimePickerContext();
 
   const { data } = useGetAvailableTreatmentSessionHoursSuspenseQuery({
     variables: {
@@ -62,27 +39,16 @@ const TimePicker = () => {
     },
   });
 
-  let hours: Date[] = [];
+  let availableHours: Date[] = [];
 
   if (data) {
-    hours = data.getAvailableTreatmentSessionHours.map(
+    availableHours = data.getAvailableTreatmentSessionHours.map(
       (ISODate) => new Date(ISODate)
     );
   }
 
   const { limitedHours, shouldRenderShowAllSessionsButton, showAllSessions } =
-    useLimitedSessionHours(hours);
-
-  const isSelected = selectedTime !== null;
-
-  useEffect(() => {
-    if (hours.length === 0 || isSelected) {
-      return;
-    }
-
-    const newDate = hours[0]!;
-    setSelectedTime(newDate);
-  }, [hours, isSelected]);
+    useLimitedSessionHours(availableHours);
 
   if (limitedHours.length === 0) {
     return <NoAvailability />;
@@ -91,18 +57,12 @@ const TimePicker = () => {
   return (
     <>
       <BoxGridStyled>
-        {limitedHours.map((date) => (
-          <ButtonStyledPicker
-            isSelected={Boolean(
-              selectedTime &&
-                selectedTime.toTimeString() === date.toTimeString()
-            )}
-            key={date.getTime()}
-            onClick={() => setSelectedTime(date)}
-            variant="primary-outlined"
-          >
-            {format(date, "h:mm aaa")}
-          </ButtonStyledPicker>
+        {limitedHours.map((datetime, index) => (
+          <TimeButton
+            key={datetime.getTime()}
+            datetime={datetime}
+            isFirstAvailableTime={index === 0}
+          />
         ))}
       </BoxGridStyled>
       {shouldRenderShowAllSessionsButton && (

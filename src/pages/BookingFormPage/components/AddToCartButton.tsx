a0d7fdafdba9487b789a70ff175fede14ac84972
mhevyk <maxxevyk7@gmail.com>
2024-05-19 08:34:30 +0300

    Implemented add to cart button on booking form page

--- src/pages/BookingFormPage/components/AddToCartButton.tsx
+++ src/pages/BookingFormPage/components/AddToCartButton.tsx
@@ -1,11 +1,15 @@
 import ButtonWithSpinner from "@components/ButtonWithSpinner";
-import { styled } from "@mui/material";
+import { Box, Tooltip, styled, useMediaQuery } from "@mui/material";
 import { useCartStore } from "@store/cart/cartStore";
 import { useOrderStore } from "@store/order/orderStore";
 import { useGetTreatmentByIdQuery } from "@api/hooks";
+import { useState } from "react";
+import theme from "@theme/theme";
 
 const ActionButton = styled(ButtonWithSpinner)({
-  marginTop: "12px",
+  ":disabled": {
+    border: "1px solid #bbb", // TODO: add some gray color to theme
+  },
 });
 
 type AddToCartButtonProps = {
@@ -14,36 +18,58 @@ type AddToCartButtonProps = {
 
 export default function AddToCartButton({ treatmentId }: AddToCartButtonProps) {
   const addToCart = useCartStore((store) => store.addToCart);
-  const { sessionStartsAt, employee } = useOrderStore();
+  const checkSessionExists = useCartStore((store) => store.checkSessionExists);
+  const sessionStartsAt = useOrderStore((store) => store.sessionStartsAt);
+  const employee = useOrderStore((store) => store.employee);
+  const isMediumScreen = useMediaQuery(theme.breakpoints.up("md"));
+
   const { data } = useGetTreatmentByIdQuery({ variables: { treatmentId } });
   const treatment = data?.treatment;
 
-  const isButtonDisabled = !treatment || !employee || !sessionStartsAt;
+  const initialIsButtonDisabled =
+    !treatment ||
+    !employee ||
+    !sessionStartsAt ||
+    checkSessionExists(treatment.id, {
+      employeeId: employee.id,
+      sessionStartsAt,
+    });
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(
+    initialIsButtonDisabled
+  );
 
   function handleAddToCart() {
     if (isButtonDisabled) {
       return;
     }
 
-    addToCart({
-      treatment,
-      session: {
-        sessionStartsAt,
-        employeeId: employee.id,
-        employeeName: employee.name,
-      },
+    addToCart(treatment!, {
+      employeeId: employee!.id,
+      employeeName: employee!.name,
+      sessionStartsAt: sessionStartsAt!,
     });
+    setIsButtonDisabled(true);
   }
 
   return (
-    <ActionButton
-      disabled={isButtonDisabled}
-      fullWidth
-      size="small"
-      variant="primary-outlined"
-      onClick={handleAddToCart}
+    <Tooltip
+      title={isButtonDisabled ? "Session is already in the cart!" : ""}
+      arrow
+      placement={isMediumScreen ? "left" : "top"}
     >
-      Add to Cart
-    </ActionButton>
+      <Box component="span" sx={{ marginTop: "12px" }}>
+        <ActionButton
+          type="button"
+          disabled={isButtonDisabled}
+          fullWidth
+          size="small"
+          variant="primary-outlined"
+          onClick={handleAddToCart}
+        >
+          Add to Cart
+        </ActionButton>
+      </Box>
+    </Tooltip>
   );
 }
--- src/store/order/orderStore.ts
+++ src/store/order/orderStore.ts
@@ -1,14 +1,14 @@
 import { create } from "zustand";
 import { Employee } from "@api/hooks";
 
-export type TreatmentDetailsStore = {
+export type OrderStore = {
   sessionStartsAt: Date | null;
   setSessionStartsAt: (date: Date | null) => void;
   employee: Employee | null;
   setEmployee: (employees: Employee | null) => void;
 };
 
-export const useOrderStore = create<TreatmentDetailsStore>((set) => ({
+export const useOrderStore = create<OrderStore>((set) => ({
   sessionStartsAt: null,
   setSessionStartsAt: (sessionStartsAt) => set({ sessionStartsAt }),
   employee: null,

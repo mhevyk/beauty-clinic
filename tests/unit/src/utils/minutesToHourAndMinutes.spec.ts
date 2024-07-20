757bcf70ac262141ba9005c7df562cad95267aee
mhevyk <maxxevyk7@gmail.com>
2024-07-20 10:15:34 +0300

    Fix naming conventions

--- tests/unit/src/utils/minutesToHourAndMinutes.spec.ts
+++ tests/unit/src/utils/minutesToHourAndMinutes.spec.ts
@@ -1,23 +1,23 @@
 import minutesToHourAndMinutes from "@/utils/minutesToHourAndMinutes";
 
 describe("minutesToHourAndMinutes", () => {
-  test("converts 0 minutes correctly", () => {
+  it("should convert 0 minutes correctly", () => {
     expect(minutesToHourAndMinutes(0)).toBe("0 hr");
   });
 
-  test("converts minutes less than an hour correctly", () => {
+  it("should convert minutes less than an hour correctly", () => {
     expect(minutesToHourAndMinutes(5)).toBe("0 hr 5 min");
     expect(minutesToHourAndMinutes(25)).toBe("0 hr 25 min");
     expect(minutesToHourAndMinutes(45)).toBe("0 hr 45 min");
   });
 
-  test("converts exactly one hour correctly", () => {
+  it("should convert exactly one hour correctly", () => {
     expect(minutesToHourAndMinutes(60)).toBe("1 hr");
     expect(minutesToHourAndMinutes(120)).toBe("2 hr");
     expect(minutesToHourAndMinutes(240)).toBe("4 hr");
   });
 
-  test("converts more than an hour correctly", () => {
+  it("should convert more than an hour correctly", () => {
     expect(minutesToHourAndMinutes(75)).toBe("1 hr 15 min");
     expect(minutesToHourAndMinutes(160)).toBe("2 hr 40 min");
     expect(minutesToHourAndMinutes(315)).toBe("5 hr 15 min");

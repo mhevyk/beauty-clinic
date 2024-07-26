a074f9b28b14ddb385346badb2e86e122694738e
mhevyk <maxxevyk7@gmail.com>
2024-07-26 10:23:59 +0300

    Add tests for ContactDetails

---
+++ tests/unit/src/pages/ContactDetails.spec.tsx
@@ -0,0 +1,19 @@
+import { screen } from "@testing-library/react";
+
+import renderWithProviders from "@tests/unit/utils/renderWithProviders";
+
+import ContactDetails from "@/layouts/footer/components/ContactDetails";
+
+jest.mock("@/layouts/footer/components/ContactForm", () => ({
+  __esModule: true,
+  default: () => <div data-testid="contact-form">Contact form</div>,
+}));
+
+describe("<ContactDetails />", () => {
+  it("should render correctly", () => {
+    renderWithProviders(<ContactDetails />);
+
+    const contactForm = screen.getByTestId("contact-form");
+    expect(contactForm).toBeInTheDocument();
+  });
+});

d38c1a5af8e6efd19c042b60feb68496cddc3a78
mhevyk <maxxevyk7@gmail.com>
2024-08-23 21:07:56 +0300

    Add react-router-dom routes

--- src/routes/guestRoutes.tsx
+++ src/routes/guestRoutes.tsx
@@ -4,11 +4,21 @@ import { RouteObject } from "react-router-dom";
 import AppSuspense from "@/routes/helpers/AppSuspense";
 
 const HomePage = lazy(() => import("@/pages/home/HomePage.tsx"));
-const TreatmentsPage = lazy(() => import("@/pages/treatments/TreatmentsPage.tsx"));
-const BookingFormPage = lazy(() => import("@/pages/booking-form/BookingFormPage.tsx"));
-const BookSessionPage = lazy(() => import("@/pages/book-session/BookSessionPage.tsx"));
+const TreatmentsPage = lazy(
+  () => import("@/pages/treatments/TreatmentsPage.tsx")
+);
+const BookingFormPage = lazy(
+  () => import("@/pages/booking-form/BookingFormPage.tsx")
+);
+const BookSessionPage = lazy(
+  () => import("@/pages/book-session/BookSessionPage.tsx")
+);
 const PostPage = lazy(() => import("@/pages/post/PostPage.tsx"));
 const CartPage = lazy(() => import("@/pages/cart/CartPage.tsx"));
+const CreatePostPage = lazy(
+  () => import("@/pages/create-post/CreatePostPage.tsx")
+);
+const EditPostPage = lazy(() => import("@/pages/edit-post/EditPostPage.tsx"));
 
 const guestRoutes: RouteObject[] = [
   {
@@ -35,6 +45,9 @@ const guestRoutes: RouteObject[] = [
         ],
       },
       { path: "cart", children: [{ index: true, element: <CartPage /> }] },
+      // TODO: refactor routes to make it more fancy
+      { path: "posts/create", element: <CreatePostPage /> },
+      { path: "posts/:postId/edit", element: <EditPostPage /> },
     ],
   },
 ];

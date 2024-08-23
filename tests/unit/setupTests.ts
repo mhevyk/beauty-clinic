36ae16be1eba5b6232b45e804f46191b29bb9aa6
mhevyk <maxxevyk7@gmail.com>
2024-08-23 21:05:41 +0300

    Fixed test setup

--- tests/unit/setupTests.ts
+++ tests/unit/setupTests.ts
@@ -1,9 +1,23 @@
 import { cleanup } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
+import { TextEncoder } from "util";
 
 afterEach(() => {
   cleanup();
   jest.clearAllMocks();
   jest.restoreAllMocks();
+});
+
+global.TextEncoder = TextEncoder;
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
 });

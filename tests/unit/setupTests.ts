1f2dd27a99325cfaae8bfe5e8d4f5803f0864ab0
mhevyk <maxxevyk7@gmail.com>
2024-08-23 21:19:53 +0300

    Fix tests

--- tests/unit/setupTests.ts
+++ tests/unit/setupTests.ts
@@ -10,14 +10,3 @@ afterEach(() => {
 });
 
 global.TextEncoder = TextEncoder;
-
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});

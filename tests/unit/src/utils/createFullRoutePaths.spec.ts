54e0477ea367d08f9dc6499d93cf827580a2f3b9
RossWebFS <shimon.vip2006@gmail.com>
2024-09-24 20:17:42 +0300

    Improved createFullRoutePaths tests

--- tests/unit/src/utils/createFullRoutePaths.spec.ts
+++ tests/unit/src/utils/createFullRoutePaths.spec.ts
@@ -1,3 +1,4 @@
+import { RoutePaths } from "@/constants/routePaths";
 import createFullRoutePaths from "@/utils/create-full-route-paths/createFullRoutePaths";
 
 const pathsTree = {
@@ -13,14 +14,30 @@ const pathsTree = {
       path: ":treatmentId",
     },
   },
+  posts: {
+    path: "/posts",
+    details: {
+      path: ":postId",
+    },
+    create: {
+      path: "create",
+    },
+    edit: {
+      path: ":postId/edit",
+    },
+  },
   any: {
     path: "*",
   },
 };
 
-const routePaths = createFullRoutePaths(pathsTree) as typeof pathsTree;
-
 describe("createFullRoutePaths", () => {
+  let routePaths: typeof pathsTree;
+
+  beforeEach(() => {
+    routePaths = createFullRoutePaths(pathsTree) as typeof pathsTree;
+  });
+
   it("should return correct root path", () => {
     expect(routePaths.home.path).toBe("/");
   });
@@ -28,18 +45,17 @@ describe("createFullRoutePaths", () => {
     expect(routePaths.treatments.path).toBe("/treatments");
   });
   it("should return correct path for nested route with params", () => {
+    expect(routePaths.posts.path).toBe("/posts");
+    expect(routePaths.posts.create.path).toBe("/posts/create");
+    expect(routePaths.posts.edit.path).toBe("/posts/:postId/edit");
+
     expect(routePaths.bookingForm.path).toBe("booking-form");
     expect(routePaths.bookingForm.details.path).toBe(
       "/booking-form/:treatmentId"
     );
   });
   it("should return correct path for non-existed route", () => {
-    const routePaths = createFullRoutePaths(pathsTree) as Record<
-      string,
-      { path: string }
-    >;
-
-    expect(routePaths.nlkn).toBe("*");
+    expect((routePaths as RoutePaths).nlkn).toBe("*");
     expect(routePaths.any?.path).toBe("*");
   });
 });

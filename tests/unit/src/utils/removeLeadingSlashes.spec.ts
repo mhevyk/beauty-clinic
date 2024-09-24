e6b089f872a88b0f9915796b36dd2edceffeb014
RossWebFS <shimon.vip2006@gmail.com>
2024-09-24 18:41:18 +0300

    Improved removeLeadingSlashes tests

--- tests/unit/src/utils/removeLeadingSlashes.spec.ts
+++ tests/unit/src/utils/removeLeadingSlashes.spec.ts
@@ -1,7 +1,17 @@
 import removeLeadingSlashes from "@/utils/remove-leading-slashes/removeLeadingSlashes";
 
-describe("removeLeadingSlashes", () => {
-  it("should remove slash in the end", () => {
+describe("removeLeadingSlashes()", () => {
+  it("should remove slash in the start", () => {
     expect(removeLeadingSlashes("/about")).toBe("about");
   });
+  it("should remove all slashes if there are only slashes", () => {
+    expect(removeLeadingSlashes("////")).toBe("");
+  })
+  it("shouldn't remove last slashes", () => {
+    expect(removeLeadingSlashes("about/")).toBe("about/")
+    expect(removeLeadingSlashes("about///")).toBe("about///")
+  })
+  it("should return an empty string", () => {
+    expect(removeLeadingSlashes("")).toBe("");
+  })
 });

31753b4841a6ee384f62033652e46bd12b747e11
mhevyk <maxxevyk7@gmail.com>
2024-09-29 12:34:16 +0300

    Add LikeWidget staff

---
+++ src/pages/post/components/like-widget/LikeWidgetSkeleton.tsx
@@ -0,0 +1,18 @@
+import { IconButton } from "@mui/material";
+
+import {
+  LikeButtonContainer,
+  LikeIcon,
+  SkeletonLikesCount,
+} from "@/pages/post/components/like-button/LikeButton.styles";
+
+export default function LikeWidgetSkeleton() {
+  return (
+    <LikeButtonContainer data-testid="like-skeleton">
+      <SkeletonLikesCount />
+      <IconButton disableRipple>
+        <LikeIcon isDisabled />
+      </IconButton>
+    </LikeButtonContainer>
+  );
+}

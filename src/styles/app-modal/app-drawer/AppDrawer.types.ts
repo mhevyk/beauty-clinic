c58d966944b3a721a89336601f81a3291b655769
mhevyk <maxxevyk7@gmail.com>
2025-02-01 21:50:05 +0200

    Sketch up AppDrawer

---
+++ src/styles/app-modal/app-drawer/AppDrawer.types.ts
@@ -0,0 +1,7 @@
+import { AppModalConfig } from "@/styles/app-modal/AppModalWrapper.types";
+
+export type AppDrawerConfig = AppModalConfig & {};
+
+export type AppDrawerProps = {
+  config: AppDrawerConfig;
+};
